5b49fd3301355bbeb69250feeb3bc8b3860b1a99
eisenburg <isenburg-eliana@ku.edu>
2024-01-26 12:17:33 -0600

    implement table-merge algorithm

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from fishing_rod import ROD_ALIKRI, ROD_ARGONIAN, ROD_DEFAULT, ROD_DWARVEN
+from typing import Union
+
+
+def merge_tables(table_1: dict[str, Union[float, int]], chance_1: int, table_2: dict[str, Union[float, int]],
+                 chance_2: int) -> dict[str, Union[float, int]]:
+    out_table = {}
+    for entry, chance in table_1.items():
+        if entry not in out_table:
+            out_table[entry] = 0
+        out_table[entry] += chance * chance_1 / 100
+    for entry, chance in table_2.items():
+        if entry not in out_table:
+            out_table[entry] = 0
+        out_table[entry] += chance * chance_2 / 100
+    return out_table
+
+
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
